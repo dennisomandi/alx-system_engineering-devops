6a21a034ac4d797c89ba6ff5fa5adc2eda4bce8a
dennisomandi <ormadydennis@gmail.com>
2022-09-17 11:16:09 +0300

    new changes

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
-# creates a file in the /tmp directory
-file { '/tmp/holberton':
-  ensure  => 'file',
+# Create a file in /tmp
+
+file { 'Alx':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+  path    => '/tmp/school',
 }
